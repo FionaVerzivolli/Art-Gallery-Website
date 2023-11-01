379ea8b04e0b34fee02d4d081d9df08e50214042
FionaV2400 <139073560+FionaVerzivolli@users.noreply.github.com>
2023-11-01 15:35:06 -0400

    Create artwebsite.js

---
+++ artwebsite.js
@@ -0,0 +1,16 @@
+function hidePreloader() {
+    var preloader = document.getElementById('preloader');
+    preloader.style.opacity = 0;
+
+    setTimeout(function () {
+        preloader.style.display = 'none';
+        var textContent = document.querySelectorAll('.hide-content');
+
+        textContent.forEach(function (element) {
+            element.classList.remove('hide-content');
+            element.classList.add('show-content');
+        });
+    }, 50); 
+}
+
+setTimeout(hidePreloader, 3000);
