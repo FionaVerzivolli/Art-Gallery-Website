262f21a1b085b0ad5b4dc35a54033d755a28a0ef
FionaV2400 <139073560+FionaVerzivolli@users.noreply.github.com>
2023-11-01 16:08:29 -0400

    Update artwebsite.js

--- artwebsite.js
+++ artwebsite.js
@@ -1,16 +1,20 @@
 function hidePreloader() {
     var preloader = document.getElementById('preloader');
-    preloader.style.opacity = 0;
+    var content = document.querySelectorAll('.hide-content');
 
     setTimeout(function () {
-        preloader.style.display = 'none';
-        var textContent = document.querySelectorAll('.hide-content');
-
-        textContent.forEach(function (element) {
+        preloader.style.opacity = 0; 
+        preloader.style.zIndex = -1;
+        preloader.style.transition = 'opacity 1s ease-out'; 
+        content.forEach(function (element) {
             element.classList.remove('hide-content');
             element.classList.add('show-content');
         });
-    }, 50); 
+    }, 2000); 
 }
 
-setTimeout(hidePreloader, 2500);
+hidePreloader();
+setTimeout(hidePreloader, 3000);
+
+
+
